41cecf8a12a849d7691f758fb84b4a10e7e5e9df
Nightling <nightling@lw.lan>
2015-04-12 13:33:22 +0200

    Fix audio on some systems, pass localtime

--- skype/entrypoint.sh
+++ skype/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -e
 useradd -d /home/skype -u ${XUID} skype 2> /dev/null
+chown skype: /home/skype
 su skype -c "skype $@"
--- skype/skype.sh
+++ skype/skype.sh
@@ -5,9 +5,9 @@ docker rm $CONTAINER_ID 2> /dev/null
 
 docker run -d -h skype --name $CONTAINER_ID \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=unix${DISPLAY} \
+-v /etc/localtime:/etc/localtime:ro \
 -v /etc/machine-id:/etc/machine-id:ro \
--v /run/user/${UID}/pulse:/run/user/${UID}/pulse:ro \
--v ${HOME}/.config/pulse:/home/skype/.config/pulse:ro \
+-v /run/user/${UID}/pulse:/run/user/pulse:ro \
 -v ${HOME}/.Skype:/home/skype/.Skype \
 -v ${HOME}/Downloads:/home/skype/Downloads \
 -e XUID=${UID} nightling/skype
