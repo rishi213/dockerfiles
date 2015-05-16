9cf91151ede7b5be9f61e18d060365d5ba2cf508
Nightling <nightling@lw.lan>
2015-05-16 18:56:08 +0200

    Publish Skype's port(s)

--- skype/skype.sh
+++ skype/skype.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 CONTAINER_ID="skype-${UID}"
+CONFIG="${HOME}/.Skype/shared.xml"
+OPTS="-e XUID=${UID}"
+
+if [ -f "$CONFIG" ]; then
+	PORTS=`egrep -o 'Port>[0-9]+' "$CONFIG" | egrep -o '[0-9]+' | uniq | \
+awk '{printf " -p %1$d:%1$d -p %1$d:%1$d/udp", $1}'`
+	OPTS="${OPTS} ${PORTS}"
+fi
 
 docker rm $CONTAINER_ID 2> /dev/null
 
@@ -10,4 +18,4 @@ docker run -d -h skype --name $CONTAINER_ID \
 -v /run/user/${UID}/pulse:/run/user/pulse:ro \
 -v ${HOME}/.Skype:/home/skype/.Skype \
 -v ${HOME}/Downloads:/home/skype/Downloads \
--e XUID=${UID} nightling/skype
+${OPTS} nightling/skype
