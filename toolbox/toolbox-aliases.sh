8898887527e2cf32efe46589a67fde9e450ba726
Nightling <nightling@lw.lan>
2015-05-18 21:22:46 +0200

    Add Toolbox

---
+++ toolbox/toolbox-aliases.sh
@@ -0,0 +1,17 @@
+toolbox() {
+	CONTAINER_ID="toolbox-${UID}-$(echo $@ | md5sum - | head -c 16)"
+
+	# Try to enter a running container...
+	docker exec -it $CONTAINER_ID bash 2> /dev/null || \
+
+	docker run -it --rm --name $CONTAINER_ID --cap-add NET_ADMIN $@ \
+	-v ${HOME}/Downloads:/root/Downloads nightling/toolbox
+}
+
+alias toolbox-sysadmin='toolbox --cap-add SYS_ADMIN'
+
+alias toolbox-host='toolbox-sysadmin --net host --ipc host --pid host -v /:/mnt/host'
+
+toolbox-attach() {
+	toolbox-sysadmin --net "container:$1" --ipc "container:$1" ${@:2}
+}
--- weevely/weevely.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-WORKDIR=/tmp/weevely-${UID}
-
-mkdir -p -m 0777 ${WORKDIR}
-echo "[#] ${WORKDIR}"
-
-docker run -it --rm -v ${WORKDIR}:/home/weevely nightling/weevely $@
