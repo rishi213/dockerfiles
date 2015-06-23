4f6035e2ac1197f09edd24d176b2fa3bbe1a636f
Nightling <nightling@lw.lan>
2015-06-23 04:05:58 +0200

    Upgrade Toolbox

--- toolbox/toolbox-aliases.sh
+++ toolbox/toolbox-aliases.sh
@@ -2,10 +2,10 @@ toolbox() {
 	CONTAINER_ID="toolbox-${UID}-$(echo $@ | md5sum - | head -c 16)"
 
 	# Try to enter a running container...
-	docker exec -it $CONTAINER_ID bash 2> /dev/null || \
+	docker exec -it "$CONTAINER_ID" bash 2> /dev/null || \
 
-	docker run -it --rm --name $CONTAINER_ID --cap-add NET_ADMIN $@ \
-	-v ${HOME}/Downloads:/root/Downloads nightling/toolbox
+	docker run -it --rm --name "$CONTAINER_ID" --cap-add NET_ADMIN $@ \
+	-v "${HOME}/Downloads:/root/Downloads" nightling/toolbox
 }
 
 alias toolbox-sysadmin='toolbox --cap-add SYS_ADMIN'
